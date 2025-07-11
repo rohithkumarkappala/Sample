667fa0b8609df387e2780f60433cd3bdf040bb25
rohithkumarkappala <rohithkumark2005@gmail.com>
2025-07-11 10:39:08 +0530

    Edit conflict.cpp again in main

--- conflict.cpp
+++ conflict.cpp
@@ -1,8 +1,7 @@
-// file: conflict.cpp
 #include<iostream>
 using namespace std;
 
 int main() {
-    cout << "Hello from MAIN branch" << endl;
+    cout << "Hello from MAIN branch - updated again" << endl;
     return 0;
 }
